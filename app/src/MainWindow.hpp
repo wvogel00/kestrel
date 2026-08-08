21a6a598a42f39ad2b123cb5d18e4b4b6c208872
Wataru TORII <eagle.hokkaido2317@gmail.com>
2026-08-08 14:32:49 +0900

    feat: add display and export shortcuts

--- app/src/MainWindow.hpp
+++ app/src/MainWindow.hpp
@@ -4,8 +4,17 @@
 
 #include <kestrel/model/ModelSpec.hpp>
 
+namespace kestrel::occt {
+class Viewer;
+}
+
 class MainWindow final : public QMainWindow {
 public:
     explicit MainWindow(const kestrel::model::NodeSpec& model,
                         QWidget* parent = nullptr);
+
+private:
+    void exportModel();
+
+    kestrel::occt::Viewer* viewer_ = nullptr;
 };
