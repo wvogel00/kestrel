499090833b050d6c9e5be95a558c169041a16f12
Wataru TORII <eagle.hokkaido2317@gmail.com>
2026-08-08 14:20:50 +0900

    m3: add native recursive model specification

--- native/include/kestrel/model/ModelSpec.hpp
+++ native/include/kestrel/model/ModelSpec.hpp
@@ -1,11 +1,37 @@
 #pragma once
 
+#include <memory>
+
 namespace kestrel::model {
 
-struct BoxSpec {
-    double widthMm;
-    double depthMm;
-    double heightMm;
+enum class NodeType {
+    Box,
+    Cylinder,
+    Translate,
+    Union,
+    Cut,
+};
+
+struct NodeSpec {
+    NodeType type;
+
+    // Box
+    double widthMm = 0.0;
+    double depthMm = 0.0;
+    double heightMm = 0.0;
+
+    // Cylinder
+    double radiusMm = 0.0;
+
+    // Translate
+    double xMm = 0.0;
+    double yMm = 0.0;
+    double zMm = 0.0;
+
+    // Recursive children
+    std::shared_ptr<NodeSpec> child;
+    std::shared_ptr<NodeSpec> left;
+    std::shared_ptr<NodeSpec> right;
 };
 
 } // namespace kestrel::model
