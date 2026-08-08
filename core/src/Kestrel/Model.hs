7c695e8b76528ea246c1a0d856e12e1467bfc566
Wataru TORII <eagle.hokkaido2317@gmail.com>
2026-08-08 14:20:33 +0900

    m3: encode recursive geometry IR v2

--- core/src/Kestrel/Model.hs
+++ core/src/Kestrel/Model.hs
@@ -4,36 +4,85 @@ module Kestrel.Model
     , encodeModelJson
     ) where
 
-import Kestrel.Geometry (Primitive(..))
+import Kestrel.Geometry (Geometry(..))
 import Kestrel.Units (toMillimetres)
 
--- | Root of a Kestrel parametric model.
 newtype Model = Model
-    { rootPrimitive :: Primitive
+    { rootGeometry :: Geometry
     }
     deriving (Eq, Show)
 
--- | Lift a primitive into a complete model.
-model :: Primitive -> Model
+model :: Geometry -> Model
 model = Model
 
--- | Encode the current minimal IR as JSON.
---
--- This encoder intentionally has no external package dependencies in M2.
--- It will be replaced by a versioned IR codec once the AST grows beyond
--- primitives.
 encodeModelJson :: Model -> String
-encodeModelJson (Model primitive) =
-    case primitive of
+encodeModelJson (Model root) =
+    concat
+        [ "{\n"
+        , "  \"version\": 2,\n"
+        , "  \"root\": "
+        , encodeGeometry 2 root
+        , "\n}\n"
+        ]
+
+encodeGeometry :: Int -> Geometry -> String
+encodeGeometry indentLevel geometry =
+    case geometry of
         Box width depth height ->
-            concat
-                [ "{\n"
-                , "  \"version\": 1,\n"
-                , "  \"root\": {\n"
-                , "    \"type\": \"box\",\n"
-                , "    \"width_mm\": ", show (toMillimetres width), ",\n"
-                , "    \"depth_mm\": ", show (toMillimetres depth), ",\n"
-                , "    \"height_mm\": ", show (toMillimetres height), "\n"
-                , "  }\n"
-                , "}\n"
+            object indentLevel
+                [ field "type" "\"box\""
+                , field "width_mm" (show $ toMillimetres width)
+                , field "depth_mm" (show $ toMillimetres depth)
+                , field "height_mm" (show $ toMillimetres height)
                 ]
+        Cylinder radius height ->
+            object indentLevel
+                [ field "type" "\"cylinder\""
+                , field "radius_mm" (show $ toMillimetres radius)
+                , field "height_mm" (show $ toMillimetres height)
+                ]
+        Translate x y z child ->
+            object indentLevel
+                [ field "type" "\"translate\""
+                , field "x_mm" (show $ toMillimetres x)
+                , field "y_mm" (show $ toMillimetres y)
+                , field "z_mm" (show $ toMillimetres z)
+                , field "child" (encodeGeometry (indentLevel + 2) child)
+                ]
+        Union left right ->
+            binaryNode indentLevel "union" left right
+        Cut left right ->
+            binaryNode indentLevel "cut" left right
+
+binaryNode :: Int -> String -> Geometry -> Geometry -> String
+binaryNode indentLevel nodeType left right =
+    object indentLevel
+        [ field "type" (show nodeType)
+        , field "left" (encodeGeometry (indentLevel + 2) left)
+        , field "right" (encodeGeometry (indentLevel + 2) right)
+        ]
+
+field :: String -> String -> (String, String)
+field = (,)
+
+object :: Int -> [(String, String)] -> String
+object indentLevel fields =
+    concat
+        [ "{\n"
+        , concatMap encodeField (zip [0 :: Int ..] fields)
+        , spaces indentLevel
+        , "}"
+        ]
+  where
+    encodeField (index, (name, value)) =
+        concat
+            [ spaces (indentLevel + 2)
+            , show name
+            , ": "
+            , value
+            , if index + 1 < length fields then "," else ""
+            , "\n"
+            ]
+
+spaces :: Int -> String
+spaces count = replicate count ' '
