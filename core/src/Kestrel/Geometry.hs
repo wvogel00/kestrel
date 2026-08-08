69e11f7684cda70cfbded2cc0ebd6fdd5fa7eb48
Wataru TORII <eagle.hokkaido2317@gmail.com>
2026-08-08 14:20:23 +0900

    m3: introduce recursive geometry AST

--- core/src/Kestrel/Geometry.hs
+++ core/src/Kestrel/Geometry.hs
@@ -1,19 +1,50 @@
 module Kestrel.Geometry
-    ( Primitive(..)
+    ( Geometry(..)
     , box
+    , cylinder
+    , translate
+    , union
+    , cut
     ) where
 
 import Kestrel.Units (Length)
 
--- | Geometric primitives supported by the current Kestrel IR.
-data Primitive
+-- | Geometry expression tree evaluated by the native OCCT backend.
+--
+-- M3 deliberately keeps the language small: two primitives, a transform,
+-- and two boolean operators. New CAD operations can be added as additional
+-- constructors without changing how models are composed.
+data Geometry
     = Box
         { boxWidth  :: Length
         , boxDepth  :: Length
         , boxHeight :: Length
         }
+    | Cylinder
+        { cylinderRadius :: Length
+        , cylinderHeight :: Length
+        }
+    | Translate
+        { translateX     :: Length
+        , translateY     :: Length
+        , translateZ     :: Length
+        , translateChild :: Geometry
+        }
+    | Union Geometry Geometry
+    | Cut Geometry Geometry
     deriving (Eq, Show)
 
--- | Construct an axis-aligned box.
-box :: Length -> Length -> Length -> Primitive
+box :: Length -> Length -> Length -> Geometry
 box = Box
+
+cylinder :: Length -> Length -> Geometry
+cylinder = Cylinder
+
+translate :: Length -> Length -> Length -> Geometry -> Geometry
+translate = Translate
+
+union :: Geometry -> Geometry -> Geometry
+union = Union
+
+cut :: Geometry -> Geometry -> Geometry
+cut = Cut
