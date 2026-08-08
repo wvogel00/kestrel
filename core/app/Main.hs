4a79a9b8669fc52ca54e5a5ff290755342a405f0
Wataru TORII <eagle.hokkaido2317@gmail.com>
2026-08-08 14:20:42 +0900

    m3: define boolean demo model in Haskell

--- core/app/Main.hs
+++ core/app/Main.hs
@@ -2,16 +2,23 @@ module Main where
 
 import System.Environment (getArgs)
 
-import Kestrel.Geometry (box)
+import Kestrel.Geometry (box, cut, cylinder, translate)
 import Kestrel.Model (encodeModelJson, model)
 import Kestrel.Units (mm)
 
+-- | M3 demonstration model: a rectangular solid with a through-hole.
 kestrelModel =
     model $
         box
+            (mm 100)
+            (mm 60)
+            (mm 20)
+        `cut`
+        translate
             (mm 50)
             (mm 30)
-            (mm 20)
+            (mm (-5))
+            (cylinder (mm 5) (mm 30))
 
 main :: IO ()
 main = do
